febbf7e7cfc91ff2e17d8665a74f1c090fc2a142
Mohammod Naimul Islam Suvon <51947096+Mdnaimulislam@users.noreply.github.com>
2022-09-01 01:41:17 +0100

    loading trasformer

---
+++ tabular_transformers.py
@@ -0,0 +1,77 @@
+from torch import nn
+from transformers import (
+    BertForSequenceClassification
+)
+from transformers.modeling_bert import BERT_INPUTS_DOCSTRING
+from transformers.file_utils import add_start_docstrings_to_callable
+from .tabular_combiner import TabularFeatCombiner
+from .tabular_config import TabularConfig
+from .layer_utils import MLP, calc_mlp_dims, hf_loss_func
+
+
+class BertWithTabular(BertForSequenceClassification):
+
+    def __init__(self, hf_model_config):
+        super().__init__(hf_model_config)
+        tabular_config = hf_model_config.tabular_config
+        if type(tabular_config) is dict:  # when loading from saved model
+            tabular_config = TabularConfig(**tabular_config)
+        else:
+            self.config.tabular_config = tabular_config.__dict__
+
+        tabular_config.text_feat_dim = hf_model_config.hidden_size
+        tabular_config.hidden_dropout_prob = hf_model_config.hidden_dropout_prob
+        self.tabular_combiner = TabularFeatCombiner(tabular_config)
+        self.num_labels = tabular_config.num_labels
+        combined_feat_dim = self.tabular_combiner.final_out_dim
+        if tabular_config.use_simple_classifier:
+            self.tabular_classifier = nn.Linear(combined_feat_dim,
+                                                tabular_config.num_labels)
+        else:
+            dims = calc_mlp_dims(combined_feat_dim,
+                                 division=tabular_config.mlp_division,
+                                 output_dim=tabular_config.num_labels)
+            self.tabular_classifier = MLP(combined_feat_dim,
+                                          tabular_config.num_labels,
+                                          num_hidden_lyr=len(dims),
+                                          dropout_prob=tabular_config.mlp_dropout,
+                                          hidden_channels=dims,
+                                          bn=True)
+
+    @add_start_docstrings_to_callable(BERT_INPUTS_DOCSTRING.format("(batch_size, sequence_length)"))
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        class_weights=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        cat_feats=None,
+        numerical_feats=None
+    ):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        combined_feats = self.tabular_combiner(pooled_output,
+                                               cat_feats,
+                                               numerical_feats)
+        loss, logits, classifier_layer_outputs = hf_loss_func(combined_feats,
+                                                              self.tabular_classifier,
+                                                              labels,
+                                                              self.num_labels,
+                                                              class_weights)
+        return loss, logits, classifier_layer_outputs
